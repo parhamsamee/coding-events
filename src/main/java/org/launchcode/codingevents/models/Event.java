c126806a7631f24b17a11974a2442214d8fabcef
parhamsamee <hallsamee@gmail.com>
2021-03-03 16:36:03 -0600

    12.3.2. Add a Unique Id

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,42 +1,46 @@
 package org.launchcode.codingevents.models;
 
-public class Event {
+import java.util.Objects;
 
+public class Event {
     //Variables
+    private int id;
+    private static int nextId = 1;
 
     private String name;
     private String description;
-
     //Constructors
-
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId;
+        nextId++;
     }
-
     //Methods
-
-    @Override
-    public String toString() {
-        return name;
-    }
-
-
     //Getters && Setters
 
-    public String getName() {
-        return name;
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public int getId() { return id; }
+
+    //custom methods
+    @Override
+    public String toString() { return name; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    @Override
+    public int hashCode() { return Objects.hash(id); }
 }
