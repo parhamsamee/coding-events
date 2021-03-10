853d64de1ea825db9af309c3c42396e5f95cca6e
parhamsamee <hallsamee@gmail.com>
2021-03-10 11:40:04 -0600

    13.3 adding model validation (validation errors)

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,9 @@
 package org.launchcode.codingevents.models;
 
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
@@ -7,12 +11,21 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+    @NotBlank
+    @Size(min = 3, max = 50, message = "Name must be between 2 and 50 characters.")
     private String name;
+
+    @Size(max = 500, message = "Description too long!")
     private String description;
+
+    @NotBlank
+    @Email(message = "Invalid email. Try again. ")
+    private String contactEmail;
     //Constructors
-    public Event(String name, String description) {
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
     }
@@ -26,6 +39,10 @@ public class Event {
     public String getDescription() { return description; }
 
     public void setDescription(String description) { this.description = description; }
+
+    public String getContactEmail() { return contactEmail; }
+
+    public void setContactEmail(String contactEmail) { this.contactEmail = contactEmail; }
 
     public int getId() { return id; }
 
